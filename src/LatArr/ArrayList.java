4f4bed6b5c3cc5060da1400f176773c23bb15d7e
arcadedelux <redredemtion101@gmail.com>
2018-01-09 14:39:50 +0700

    Latihan Array Menggunakan generik

--- src/LatArr/ArrayList.java
+++ src/LatArr/ArrayList.java
@@ -6,15 +6,15 @@ import java.math.BigDecimal;
 public class ArrayList{
 
 public static void main (String [] args){
-List values = new java.util.ArrayList();
-values.add("Arief Setya Budi");
+List <Integer> values = new java.util.ArrayList();
 values.add(1);
-values.add(false);
-values.add(true);
-values.add('A');
-values.add(100f);
-values.add(0.1d);
-values.add(new BigDecimal(1000000));
+values.add(2);
+values.add(3);
+values.add(4);
+values.add(5);
+values.add(5);
+values.add(6);
+values.add(7);
 
 for(int i= 0;i < values.size();i++){
 System.out.println(values.get(i));
